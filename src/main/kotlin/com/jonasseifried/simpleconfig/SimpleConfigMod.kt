1623553e8f49aec04550d42d1d04680f1c4af16a
JonasSeifried <69268825+JonasSeifried@users.noreply.github.com>
2024-02-09 15:09:37 +0100

    housekeeping

--- src/main/kotlin/com/jonasseifried/simpleconfig/SimpleConfigMod.kt
+++ src/main/kotlin/com/jonasseifried/simpleconfig/SimpleConfigMod.kt
@@ -1,7 +1,5 @@
 package com.jonasseifried.simpleconfig
 
-import com.jonasseifried.simpleconfig.SimpleConfigMod.MOD_ID
-import kotlinx.serialization.Serializable
 import net.fabricmc.api.ModInitializer
 import org.slf4j.LoggerFactory
 
@@ -10,28 +8,7 @@ object SimpleConfigMod : ModInitializer {
     private val logger = LoggerFactory.getLogger(MOD_ID)
 
     override fun onInitialize() {
-        // This code runs as soon as Minecraft is in a mod-load-ready state.
-        // However, some things (like resources) may still be uninitialized.
-        // Proceed with mild caution.
-
-        MyConfig.patch(loadBefore = true) {
-            test = 1.99999
-            hello = "wowowow"
-        }
-        MyConfig.patch {
-            test = 2.5
-        }
         registerCommands()
         logger.info("Config Initialized")
-    }
-}
-
-object MyConfig : SimpleConfig<MyConfig.MyData>(defaultValue = MyData()) {
-    override var fileName = MOD_ID
-
-    @Serializable
-    class MyData {
-        var test = 2.0
-        var hello = "world"
     }
 }
